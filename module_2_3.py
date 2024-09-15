2b0700eec4baf0bb9b717734dd2498cf0d20641b
Alena <89519703610alena@gmail.com>
2024-09-15 21:37:29 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,27 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i + 1 < len(my_list):
+      i = 0
+      if my_list [i] > 0 :
+          print (my_list [i])
+          break
+while i + 1 < len(my_list):
+    i = i + 1
+    if my_list[i] > 0:
+        print(my_list[i])
+    else:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
